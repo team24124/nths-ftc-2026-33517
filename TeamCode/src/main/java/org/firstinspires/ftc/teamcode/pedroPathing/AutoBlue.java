ee4ecaacfc70d70991153d221b80404da70df0c1
niceghy <louissu2009@gmail.com>
2025-11-22 17:01:41 -0700

    Remove duplicate import statement for AutoBlue

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/AutoBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/AutoBlue.java
@@ -7,7 +7,6 @@ import com.pedropathing.paths.Path;
 import com.pedropathing.paths.PathChain;
 import com.pedropathing.util.Timer;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import  com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 @Autonomous(name = "Auto Blue", group = "Examples")
