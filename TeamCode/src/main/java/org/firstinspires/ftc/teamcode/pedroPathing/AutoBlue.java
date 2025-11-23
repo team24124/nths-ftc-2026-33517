fa90a33d7ee6a8c58a6156072ca0b8c43759d690
Your Name <your.email@example.com>
2025-11-22 19:03:59 -0700

    Completed the full auto with new paths set into different pathchains.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/AutoBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/AutoBlue.java
@@ -16,38 +16,60 @@ public class AutoBlue extends OpMode {
 
     private int pathState;
 
-    private final Pose startPose = new Pose(24, 24, Math.toRadians(315)),
-            middlePose = new Pose(72, 72, Math.toRadians(45)),
-            ballsPose = new Pose(108, 36, Math.toRadians(0)),
-            ballsPose2 = new Pose (108, 60, Math.toRadians(0)),
-            ballsPose3 = new Pose(108, 84, Math.toRadians(0));
+    private final Pose startPose = new Pose(24, 120, Math.toRadians(315)),
+            middlePose = new Pose(72, 72, Math.toRadians(135)),
+            ballsPose = new Pose(96, 84, Math.toRadians(0)),
+            ballsCapture = new Pose(120, 84, Math.toRadians(0)),
+            ballsPose2 = new Pose (96, 60, Math.toRadians(0)),
+            ballsCapture2 = new Pose(120, 60, Math.toRadians(0)),
+            ballsPose3 = new Pose(96, 36, Math.toRadians(0)),
+            ballsCapture3 = new Pose(120, 36, Math.toRadians(0)),
+            base = new Pose(105, 33, Math.toRadians(0));
 
-    private Path move1, move2, move3, move4, move5, move6;
+    private PathChain toMiddle, topBalls, middleBalls, bottomBalls, toBase;
 
     public void buildPaths() {
-        // Move from the front of the blue goal to the balls on the right side
-        move1 = new Path(new BezierLine(startPose, ballsPose));
-        move1.setLinearHeadingInterpolation(startPose.getHeading(), ballsPose.getHeading());
+        // Move to middle to shoot preloaded balls
+        toMiddle = follower.pathBuilder()
+                .addPath(new BezierLine(startPose, middlePose))
+                .setLinearHeadingInterpolation(startPose.getHeading(), middlePose.getHeading())
+                .build();
 
-        // Move to the middle
-        move2 = new Path(new BezierLine(ballsPose, middlePose));
-        move2.setLinearHeadingInterpolation(ballsPose.getHeading(), middlePose.getHeading());
+        // Move from middle to top set of balls, intake, and return to middle
+        topBalls = follower.pathBuilder()
+                .addPath(new BezierLine(middlePose, ballsPose))
+                .setLinearHeadingInterpolation(middlePose.getHeading(), ballsPose.getHeading())
+                .addPath(new BezierLine(ballsPose, ballsCapture))
+                .setLinearHeadingInterpolation(ballsPose.getHeading(), ballsCapture.getHeading())
+                .addPath(new BezierLine(ballsCapture, middlePose))
+                .setLinearHeadingInterpolation(ballsCapture.getHeading(), middlePose.getHeading())
+                .build();
 
-        // Move from the middle to the second set of balls
-        move3 = new Path(new BezierLine(middlePose, ballsPose2));
-        move3.setLinearHeadingInterpolation(middlePose.getHeading(), ballsPose2.getHeading());
+        // Move from middle to middle set of balls, intake, and return to middle
+        middleBalls = follower.pathBuilder()
+                .addPath(new BezierLine(middlePose, ballsPose2))
+                .setLinearHeadingInterpolation(middlePose.getHeading(), ballsPose2.getHeading())
+                .addPath(new BezierLine(ballsPose2, ballsCapture2))
+                .setLinearHeadingInterpolation(ballsPose2.getHeading(), ballsCapture2.getHeading())
+                .addPath(new BezierLine(ballsCapture2, middlePose))
+                .setLinearHeadingInterpolation(ballsCapture2.getHeading(), middlePose.getHeading())
+                .build();
 
-        // Move from the second set of balls to the middle
-        move4 = new Path(new BezierLine(ballsPose2, middlePose));
-        move4.setLinearHeadingInterpolation(ballsPose2.getHeading(), middlePose.getHeading());
+        // Move from middle to bottom set of balls, intake, and return to middle
+        bottomBalls = follower.pathBuilder()
+                .addPath(new BezierLine(middlePose, ballsPose3))
+                .setLinearHeadingInterpolation(middlePose.getHeading(), ballsPose3.getHeading())
+                .addPath(new BezierLine(ballsPose3, ballsCapture3))
+                .setLinearHeadingInterpolation(ballsPose3.getHeading(), ballsCapture3.getHeading())
+                .addPath(new BezierLine(ballsCapture3, middlePose))
+                .setLinearHeadingInterpolation(ballsCapture3.getHeading(), middlePose.getHeading())
+                .build();
 
-        // Move from the middle to the third set of balls
-        move5 = new Path(new BezierLine(middlePose, ballsPose3));
-        move5.setLinearHeadingInterpolation(middlePose.getHeading(), ballsPose3.getHeading());
-
-        // Move from the third set of balls to the middle
-        move6 = new Path(new BezierLine(ballsPose3, middlePose));
-        move6.setLinearHeadingInterpolation(ballsPose3.getHeading(), middlePose.getHeading());
+        // Move to base
+        toBase = follower.pathBuilder()
+                .addPath(new BezierLine(middlePose, base))
+                .setLinearHeadingInterpolation(middlePose.getHeading(), base.getHeading())
+                .build();
     }
 
     /** This is the main loop of the OpMode, it will run repeatedly after clicking "Play". **/
@@ -70,29 +92,61 @@ public class AutoBlue extends OpMode {
     public void autonomousPathUpdate() {
         switch (pathState) {
             case 0:
-                follower.followPath(move1);
-                checkIfBusy(1);
+                follower.followPath(toMiddle, true);
+                setPathState(1);
                 break;
             case 1:
+                // TODO: Shoot preloaded balls
                 checkIfBusy(2);
                 break;
             case 2:
-                follower.followPath(move2);
-                checkIfBusy(3);
+                follower.followPath(topBalls, true);
+                setPathState(3);
                 break;
             case 3:
+                // TODO: Shoot top balls
                 checkIfBusy(4);
                 break;
             case 4:
-                telmetry.addData("Status", "Auto Complete");
+                follower.followPath(middleBalls, true);
+                setPathState(5);
+                break;
+            case 5:
+                // TODO: Shoot middle balls
+                checkIfBusy(6);
+                break;
+            case 6:
+                follower.followPath(bottomBalls, true);
+                setPathState(7);
+                break;
+            case 7:
+                // TODO: Shoot bottom balls
+                checkIfBusy(8);
+                break;
+            case 8:
+                follower.followPath(toBase, true);
+                setPathState(9);
+                break;
+            case 9:
+                checkIfBusy(10);
+                break;
+            case 10:
+                telemetry.addData("Status", "Auto Complete");
                 break;
         }
     }
 
+    /** This method checks if the path is busy, if not: set the path state **/
     public void checkIfBusy(int state) {
         if (!follower.isBusy()) {
             setPathState(state);
         }
+    }
+
+    /** This method sets the path state**/
+    public void setPathState(int state) {
+        pathState = state;
+        pathTimer.resetTimer();
     }
 
     /** This method is called once at the init of the OpMode. **/
